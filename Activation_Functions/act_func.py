a2165068f464640707ab96aac76f30ebb698107d
dobrematei14 <d.dobre.1@student.rug.nl>
2025-07-09 17:25:18 +0200

    commented some lines in act_func.py and loss.py for clarity

--- Activation_Functions/act_func.py
+++ Activation_Functions/act_func.py
@@ -1,26 +1,26 @@
 import numpy as np
 
-class ActivationStep:
+class ActivationStep: # 0 negative, 1 positive activation
     def forward(self, inputs): 
         self.output = np.array([1 if i > 0 else 0 for i in inputs])
         return self.output
 
-class ActivationLinear:
+class ActivationLinear: # identity activation
     def forward(self, inputs):
         self.output = inputs
         return self.output
     
-class ActivationSigmoid:
+class ActivationSigmoid: #  (0,1) activation 
     def forward(self, inputs):
         self.output = 1 / (1 + np.exp(-inputs))
         return self.output
 
-class ActivationReLU:
+class ActivationReLU: # 0 negative, linear positive activation
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
         return self.output
     
-class ActivationSoftmax:
+class ActivationSoftmax: # sum to 1 probability distribution activation
     def forward(self, inputs):
         self.output = np.exp(inputs) / np.sum(np.exp(inputs), axis=1, keepdims=True)
         return self.output
--- Losses/loss.py
+++ Losses/loss.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-class Loss:
+class Loss: # base class
     def calculate(self, output, y):
-        sample_losses = self.forward(output, y)
+        sample_losses = self.forward(output, y) # calculate loss for each sample
 
         data_loss = np.mean(sample_losses)
-        return data_loss
+        return data_loss # returns the average loss across all samples
     
     
 class Loss_CategoricalCrossentropy(Loss):
     def forward(self, y_pred, y_true):
         samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) #clip to avoid log(0)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7) #clip to avoid log(0), inf or undefined
 
-        if len(y_true.shape) == 1: 
+        if len(y_true.shape) == 1:  # for integer labels like '2'
             correct_confidences = y_pred_clipped[range(samples), y_true]
-        elif len(y_true.shape) == 2:
+        elif len(y_true.shape) == 2: # for one-hot encoded labels like [0, 0, 1]
             correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
 
         return -np.log(correct_confidences)
